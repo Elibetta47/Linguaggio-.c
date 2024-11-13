7e124a1a4d218ed595e093f2ab1874fb20feba79
Elisabetta <e.borgonovo47@gmail.com>
2024-11-13 09:34:01 -0500

    Esecizio L3/S3

---
+++ esercizio S2L3/esercizio.py
@@ -0,0 +1,7 @@
+# Richiesta di imput all'utente
+citta = input("Inserisci il nome della tua citta d'origine: ")
+animale = input("Inserisci il nome del tuo animale domestico: ")
+#Generazione del nome della band
+nome_band = citta + " " + animale + " Ramones"
+#Output del nome generato
+print("Il nome della tua band è:", nome_band)
